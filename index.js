kadamwhite/block-editor-hmr

commit 2252caa97c11d8caa357de87a6b56573093a6757

Use less confusing syntax for settings.name fallback

index.js
@@ -93,14 +93,14 @@ let selectedBlockId = null;
  * Register a new or updated block.
  *
  * @param {Object}   block            The exported block module.
+ * @param {String}   block.name       Block name. May be included in configuration object.
  * @param {Object}   block.settings   Block configuration object.
- * @param {String}   block.name       Block name. May be included in configuration.
  * @param {Object[]} [block.filters]  Optional array of filters to bind.
  * @param {Object[]} [block.styles]   Optional array of block styles to bind.
  */
-export const registerBlock = ( { settings, name = settings?.name, filters, styles } ) => {
-	if ( name && settings ) {
-		blocks.registerBlockType( name, settings );
+export const registerBlock = ( { name, settings, filters, styles } ) => {
+	if ( ( name || settings.name ) && settings ) {
+		blocks.registerBlockType( ( name || settings.name ), settings );
 	}
 
 	if ( filters && Array.isArray( filters ) ) {
@@ -118,14 +118,14 @@ export const registerBlock = ( { settings, name = settings?.name, filters, style
  * Unregister an updated or removed block.
  *
  * @param {Object}   block            The exported block module.
+ * @param {String}   block.name       Block name. May be included in configuration object.
  * @param {Object}   block.settings   Block configuration object.
- * @param {String}   block.name       Block name. May be included in configuration.
  * @param {Object[]} [block.filters]  Optional array of filters to bind.
  * @param {Object[]} [block.styles]   Optional array of block styles to bind.
  */
-export const unregisterBlock = ( { settings, name = settings?.name, filters, styles } ) => {
-	if ( name && settings ) {
-		blocks.unregisterBlockType( name );
+export const unregisterBlock = ( { name, settings, filters, styles } ) => {
+	if ( ( name || settings.name ) && settings ) {
+		blocks.unregisterBlockType( ( name || settings.name ) );
 	}
 
 	if ( filters && Array.isArray( filters ) ) {

commit 10794a36900eb256360d2f6c300dafdfb375bc17

Allow autoloadBlocks to handle filters or styles without block object
Before a recent change, it was possible to use the autoloadBlocks
functionality to register filters or styles without actually exporting a
block settings object. 2252caa97 broke that functionality by causing an
uncaught type error if a file passed to autoloadBlocks didn't export a
settings object. This adds null checking around the settings object
before accessing it.

index.js
@@ -90,17 +90,17 @@ export const autoload = (
 let selectedBlockId = null;
 
 /**
- * Register a new or updated block.
+ * Register a new or updated block, filters, or style variations.
  *
  * @param {Object}   block            The exported block module.
  * @param {String}   block.name       Block name. May be included in configuration object.
- * @param {Object}   block.settings   Block configuration object.
+ * @param {Object}   [block.settings] Optional block configuration object.
  * @param {Object[]} [block.filters]  Optional array of filters to bind.
  * @param {Object[]} [block.styles]   Optional array of block styles to bind.
  */
 export const registerBlock = ( { name, settings, filters, styles } ) => {
-	if ( ( name || settings.name ) && settings ) {
-		blocks.registerBlockType( ( name || settings.name ), settings );
+	if ( ( name || settings?.name ) && settings ) {
+		blocks.registerBlockType( ( name || settings?.name ), settings );
 	}
 
 	if ( filters && Array.isArray( filters ) ) {
@@ -115,17 +115,17 @@ export const registerBlock = ( { name, settings, filters, styles } ) => {
 };
 
 /**
- * Unregister an updated or removed block.
+ * Unregister an updated or removed block, filters, or style variations.
  *
  * @param {Object}   block            The exported block module.
  * @param {String}   block.name       Block name. May be included in configuration object.
- * @param {Object}   block.settings   Block configuration object.
+ * @param {Object}   [block.settings] Optional block configuration object.
  * @param {Object[]} [block.filters]  Optional array of filters to bind.
  * @param {Object[]} [block.styles]   Optional array of block styles to bind.
  */
 export const unregisterBlock = ( { name, settings, filters, styles } ) => {
-	if ( ( name || settings.name ) && settings ) {
-		blocks.unregisterBlockType( ( name || settings.name ) );
+	if ( ( name || settings?.name ) && settings ) {
+		blocks.unregisterBlockType( ( name || settings?.name ) );
 	}
 
 	if ( filters && Array.isArray( filters ) ) {